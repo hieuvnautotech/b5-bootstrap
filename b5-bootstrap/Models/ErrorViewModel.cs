b817438311a8b6086f5fcdf6adc0e86251889ad4
hieu nguyen trung <hieuvnautotech@gmail.com>
2020-05-26 19:04:08 +0700

    Add project files.

---
+++ b5-bootstrap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace b5_bootstrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
